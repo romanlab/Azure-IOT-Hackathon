3ad0727d73cfad36385fd5d8e55c2f7fd4379bac
romanl <romanl@ironsrc.com>
2015-01-15 23:05:28 +0200

    Added climate handler

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,5 +10,10 @@ app.use(cors);
 app.use(users);
 app.use(spots);
 
+app.post('/climatedata', function(req, res, next) {
+  res.status(200).send('OK');
+
+});
+
 // Run Express
 app.listen(process.env.port || 1337);
