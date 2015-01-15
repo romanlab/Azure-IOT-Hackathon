f24368d28749c89aa566b6a453967ffec0200894
Ariel Mashraki <ariel.mashraki@ironsrc.com>
2015-01-15 20:33:05 +0200

    fix(cors): allow cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,11 @@
 var express = require('express')
   , users   = require('./lib/users')
   , spots   = require('./lib/spots')
+  , cors    = require('./lib/cors')
   , app     = module.exports = express();
 
 // App Middleware
+app.use(cors);
 app.use(users);
 app.use(spots);
 
