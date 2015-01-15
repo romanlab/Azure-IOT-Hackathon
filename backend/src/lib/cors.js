a6449b7f84af5ef248464a84c1994c4a0ca3ba92
Ariel Mashraki <ariel.mashraki@ironsrc.com>
2015-01-15 23:58:24 +0200

    foobar

--- backend/src/lib/cors.js
+++ backend/src/lib/cors.js
@@ -3,5 +3,6 @@
 module.exports = function allowCrossDomain(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   next();
 };
--- fronend/src/app.js
+++ fronend/src/app.js
@@ -1,20 +1,67 @@
+var baseUrl = 'http://localhost:1337';
+
 angular.module('autopark', ['ui.bootstrap'])
   .controller('MainCtrl', function($scope, $http) {
+    $scope.modal = false;
     $scope.blank = true;
     $scope.rowA = [];
     $scope.rowB = [];
-    $http.get('http://localhost:1337/spots')
-      .then(function(response) {
-        var spots = response.data;
-        spots.forEach(function(spot) {
-          var date = new Date(spot.parkingOut);
-          var minutes = String(date.getMinutes());
-          //one digit, yes ugly
-          spot.time = [date.getHours(), ':', minutes >> 1 ? minutes : minutes + '0'].join('');
-          return !spot.location.indexOf('A')
-            ? $scope.rowA.push(spot)
-            : $scope.rowB.push(spot);
+    $scope.carId = null;
+    $scope.troubleUsers = [];
+
+    function getSpots() {
+      return $http.get(baseUrl + '/spots')
+        .then(function(response) {
+          var spots = response.data;
+          spots.forEach(function(spot) {
+            var date = new Date(spot.parkingOut);
+            var minutes = String(date.getMinutes());
+            //one digit, yes ugly
+            if(/\d+/g.test(spot.parkingOut)) {
+              spot.time = spot.parkingOut;
+            } else {
+              spot.time = [date.getHours(), ':', minutes >> 1 ? minutes : minutes + '0'].join('');
+            }
+            return !spot.location.indexOf('A')
+              ? $scope.rowA.push(spot)
+              : $scope.rowB.push(spot);
+          });
+          $scope.blank = false;
         });
-        $scope.blank = false;
+    }
+
+    getSpots();
+
+    $http.get(baseUrl + '/users')
+      .then(function(response) {
+        response.data.map(function(user) {
+          return user.troublemaker
+            ? $scope.troubleUsers.push(user)
+            : false;
+        });
       });
+
+    $scope.updateUser = function(userId) {
+      $scope.userToUpdate = userId;
+      $scope.modal = true;
+    };
+
+    $scope.updateForm = function() {
+      $http.put(baseUrl + '/spots/' + $scope.carId,
+        { owner: $scope.userToUpdate, assigned:true, parkingOut: $scope.time
+        })
+        .then(function(resp) {
+          $scope.troubleUsers.forEach(function(user, i) {
+            if(user.id == $scope.userToUpdate) {
+              $scope.troubleUsers.splice(i, 1);
+            }
+          });
+          delete $scope.carId;
+          delete $scope.userToUpdate;
+          delete $scope.time;
+          getSpots().then(function() {
+            $scope.modal = false;
+          });
+        });
+    }
   });
